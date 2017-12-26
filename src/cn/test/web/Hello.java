f18b05f1bbf20d582568749d245c288550e3a3b4
LingYino <13707074520@163.com>
2017-12-26 11:07:44 +0800

    first commit

---
+++ src/cn/test/web/Hello.java
@@ -0,0 +1,23 @@
+package cn.test.web;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Hello extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		System.out.println("Hello,this is doGet!");
+		doPost(req, resp);
+	}
+
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		System.out.println("Hello,this is doPost!");
+		req.getRequestDispatcher("index.jsp").forward(req, resp);
+	}
+}
